c5426b2b696085ab0db8429960fea5af5bf5e0b1
Frederick Wang <hwoam@outlook.com>
2019-01-02 04:33:09 +0800

    修复TypeScript报错，引入Element-ui

--- src/background.ts
+++ src/background.ts
@@ -1,35 +1,37 @@
-'use strict'
-
-import { app, protocol, BrowserWindow } from 'electron'
+'use strict';
+import * as fs from 'fs';
+import { app, protocol, BrowserWindow, ipcMain as ipc, dialog } from 'electron';
 import {
   createProtocol,
-  installVueDevtools
-} from 'vue-cli-plugin-electron-builder/lib'
-const isDevelopment = process.env.NODE_ENV !== 'production'
+  installVueDevtools,
+} from 'vue-cli-plugin-electron-builder/lib';
+const isDevelopment = process.env.NODE_ENV !== 'production';
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let win
+let mainWindow: BrowserWindow;
 
 // Standard scheme must be registered before the app is ready
-protocol.registerStandardSchemes(['app'], { secure: true })
-function createWindow () {
+protocol.registerStandardSchemes(['app'], { secure: true });
+function createWindow() {
   // Create the browser window.
-  win = new BrowserWindow({ width: 800, height: 600 })
+  mainWindow = new BrowserWindow({ width: 800, height: 600, autoHideMenuBar: true });
 
   if (process.env.WEBPACK_DEV_SERVER_URL) {
     // Load the url of the dev server if in development mode
-    win.loadURL(process.env.WEBPACK_DEV_SERVER_URL as string)
-    if (!process.env.IS_TEST) win.webContents.openDevTools()
+    mainWindow.loadURL(process.env.WEBPACK_DEV_SERVER_URL as string);
+    if (!process.env.IS_TEST) {
+      mainWindow.webContents.openDevTools();
+    }
   } else {
-    createProtocol('app')
+    createProtocol('app');
     // Load the index.html when not in development
-    win.loadURL('app://./index.html')
+    mainWindow.loadURL('app://./index.html');
   }
 
-  win.on('closed', () => {
-    win = null
-  })
+  mainWindow.on('closed', () => {
+    (mainWindow as any) = null;
+  });
 }
 
 // Quit when all windows are closed.
@@ -37,17 +39,17 @@ app.on('window-all-closed', () => {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
-})
+});
 
 app.on('activate', () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (win === null) {
-    createWindow()
+  if (mainWindow === null) {
+    createWindow();
   }
-})
+});
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
@@ -55,22 +57,51 @@ app.on('activate', () => {
 app.on('ready', async () => {
   if (isDevelopment && !process.env.IS_TEST) {
     // Install Vue Devtools
-    await installVueDevtools()
+    await installVueDevtools();
   }
-  createWindow()
-})
+  createWindow();
+});
 
 // Exit cleanly on request from parent process in development mode.
 if (isDevelopment) {
   if (process.platform === 'win32') {
-    process.on('message', data => {
+    process.on('message', (data) => {
       if (data === 'graceful-exit') {
-        app.quit()
+        app.quit();
       }
-    })
+    });
   } else {
     process.on('SIGTERM', () => {
-      app.quit()
-    })
+      app.quit();
+    });
   }
 }
+
+ipc.on('open-file-dialog', (event: any) => {
+  dialog.showOpenDialog({
+    properties: ['openFile', 'openDirectory'],
+  }, (files) => {
+    if (files) {
+      event.sender.send('selected-directory', files);
+    }
+  });
+});
+
+ipc.on('save-dialog-txt', (event: any, data: string) => {
+  const options = {
+    title: '导出为文本文档',
+    filters: [
+      { name: '文本文档', extensions: ['txt'] },
+    ],
+  };
+  dialog.showSaveDialog(options, (filename) => {
+    if (filename) {
+      fs.writeFile(filename, data, (err) => {
+        event.sender.send('saved-file-txt', {
+          error: err,
+          path: filename,
+        });
+      });
+    }
+  });
+});
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <el-button>test</el-button>
   </div>
 </template>
 
