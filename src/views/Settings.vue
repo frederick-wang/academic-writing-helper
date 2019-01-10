fd83ef4d58ae0bb66bed88ccb5e6904741db4baa
Frederick Wang <hwoam@outlook.com>
2019-01-11 07:18:38 +0800

    ⚒ rewrite(Setting): remove a unnecessary imported package.

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -75,7 +75,7 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue, Watch } from 'vue-property-decorator';
+import { Component, Vue} from 'vue-property-decorator';
 import { Logger } from '@/Tools';
 
 @Component
