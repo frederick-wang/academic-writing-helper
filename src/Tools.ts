71c9286fd11e53613ca3ee372f1297dc0144496a
Frederick Wang <hwoam@outlook.com>
2019-01-14 05:14:45 +0800

    🛠 rewrite(Tools): 🔧 1. Use closures to hide those scattered external variables. 🔧 2. Use RegExp Constructors to simplify codes of RegExps.

--- src/Tools.ts
+++ src/Tools.ts
@@ -29,77 +29,78 @@ export const Logger = {
 };
 
 // Dict
-let junior: any;
-junior = importWordsData(require('@/assets/dict/junior.json'));
-const cet4 = importWordsData(require('@/assets/dict/cet4.json'));
-const cet6 = importWordsData(require('@/assets/dict/cet6.json'));
-const toefl = importWordsData(require('@/assets/dict/toefl.json'));
-const gre = importWordsData(require('@/assets/dict/gre.json'));
+export const Dict = (() => {
+  const importWordsData = (data: string[]): Map<string, boolean> => {
+    if (junior) {
+      return new Map(
+        data
+          .filter(v => !junior.has(v))
+          .map((v: string): [string, boolean] => [v, true])
+      );
+    }
+    return new Map(data.map((v: string): [string, boolean] => [v, true]));
+  };
 
-function importWordsData(data: string[]): Map<string, boolean> {
-  if (junior) {
-    return new Map(
-      data
-        .filter(v => !junior.has(v))
-        .map((v: string): [string, boolean] => [v, true])
-    );
-  }
-  return new Map(data.map((v: string): [string, boolean] => [v, true]));
-}
+  let junior: any;
+  junior = importWordsData(require('@/assets/dict/junior.json'));
+  const cet4 = importWordsData(require('@/assets/dict/cet4.json'));
+  const cet6 = importWordsData(require('@/assets/dict/cet6.json'));
+  const toefl = importWordsData(require('@/assets/dict/toefl.json'));
+  const gre = importWordsData(require('@/assets/dict/gre.json'));
 
-export const Dict = {
-  isCET4: (word: string) => cet4.has(word.toLowerCase()),
-  isCET6: (word: string) => cet6.has(word.toLowerCase()),
-  isToefl: (word: string) => toefl.has(word.toLowerCase()),
-  isGRE: (word: string) => gre.has(word.toLowerCase()),
-  wordBorderColor: {
-    cet4: '#DCDFE6',
-    cet6: '#E6A23C',
-    toefl: '#67C23A',
-    gre: '#409EFF'
-  },
-  wordBackgroundColor: {
-    none: 'transparent',
-    cet4: '#DCDFE680',
-    cet6: '#E6A23C80',
-    toefl: '#67C23A80',
-    gre: '#409EFF80'
-  },
-  getWordScore(word: string) {
-    if (Dict.isCET4(word)) {
-      return 1;
+  return {
+    isCET4: (word: string) => cet4.has(word.toLowerCase()),
+    isCET6: (word: string) => cet6.has(word.toLowerCase()),
+    isToefl: (word: string) => toefl.has(word.toLowerCase()),
+    isGRE: (word: string) => gre.has(word.toLowerCase()),
+    wordBorderColor: {
+      cet4: '#DCDFE6',
+      cet6: '#E6A23C',
+      toefl: '#67C23A',
+      gre: '#409EFF'
+    },
+    wordBackgroundColor: {
+      none: 'transparent',
+      cet4: '#DCDFE680',
+      cet6: '#E6A23C80',
+      toefl: '#67C23A80',
+      gre: '#409EFF80'
+    },
+    getWordScore(word: string) {
+      if (Dict.isCET4(word)) {
+        return 1;
+      }
+      if (Dict.isCET6(word)) {
+        return 9;
+      }
+      if (Dict.isToefl(word)) {
+        return 25;
+      }
+      if (Dict.isGRE(word)) {
+        return 49;
+      }
+      return 0;
+    },
+    getWordBackgroundColor(word: string) {
+      word = word.toLowerCase();
+      if (Dict.isCET4(word)) {
+        return Dict.wordBackgroundColor.cet4;
+      }
+      if (Dict.isCET6(word)) {
+        return Dict.wordBackgroundColor.cet6;
+      }
+      if (Dict.isToefl(word)) {
+        return Dict.wordBackgroundColor.toefl;
+      }
+      if (Dict.isGRE(word)) {
+        return Dict.wordBackgroundColor.gre;
+      }
+      return Dict.wordBackgroundColor.none;
     }
-    if (Dict.isCET6(word)) {
-      return 9;
-    }
-    if (Dict.isToefl(word)) {
-      return 25;
-    }
-    if (Dict.isGRE(word)) {
-      return 49;
-    }
-    return 0;
-  },
-  getWordBackgroundColor(word: string) {
-    word = word.toLowerCase();
-    if (Dict.isCET4(word)) {
-      return Dict.wordBackgroundColor.cet4;
-    }
-    if (Dict.isCET6(word)) {
-      return Dict.wordBackgroundColor.cet6;
-    }
-    if (Dict.isToefl(word)) {
-      return Dict.wordBackgroundColor.toefl;
-    }
-    if (Dict.isGRE(word)) {
-      return Dict.wordBackgroundColor.gre;
-    }
-    return Dict.wordBackgroundColor.none;
-  }
-};
+  };
+})();
 
 // Translation
-let dict: any;
 export const Translation = (() => {
   // const DATA_PATH = path.resolve(APP_PATH, 'data');
   /**
@@ -107,7 +108,7 @@ export const Translation = (() => {
    * FIXME: It seems that dict.json cannot be created in build version.
    */
   const DICT_PATH = path.resolve(APP_PATH, 'data/dict.json');
-  dict = {};
+  let dict: any = {};
 
   (async () => {
     try {
@@ -181,77 +182,72 @@ export const Translation = (() => {
 })();
 
 // Punctuation
-/**
- * 2019-1-13 06:53:36
- * TODO: These scattered external variables should be hidden, with closures.
- */
-/**
- * 2019-1-13 06:43:40
- * TODO: Use RegExp Constructors to simplify codes of next two lines.
- */
-const sentencePunctuationRegExp = /.+?(;|(\.{3}|\.)|\?|!|…|$)/;
-const sentencePunctuationRegExpG = /.+?(;|(\.{3}|\.)|\?|!|…|$)/g;
-const punctuations = new Map([
-  [':', true],
-  [',', true],
-  [';', true],
-  ['"', true],
-  ['`', true],
-  ['~', true],
-  [`'`, true],
-  ['-', true],
-  ['--', true],
-  ['\\', true],
-  ['/', true],
-  ['<', true],
-  ['>', true],
-  ['{', true],
-  ['}', true],
-  ['[', true],
-  [']', true],
-  ['+', true],
-  ['-', true],
-  ['*', true],
-  ['_', true],
-  ['(', true],
-  [')', true],
-  ['&', true],
-  ['^', true],
-  ['%', true],
-  ['$', true],
-  ['#', true],
-  ['@', true],
-  ['.', true],
-  ['?', true],
-  ['!', true],
-  ['|', true],
-  ['...', true],
-  ['…', true]
-]);
+export const Punctuation = (() => {
+  const regExpPattern = `.+?(;|(\\\.{3}|\\\.)|\\\?|!|…|$)`;
+  const sentencePunctuationRegExp = new RegExp(regExpPattern);
+  const sentencePunctuationRegExpG = new RegExp(regExpPattern, 'g');
+  const punctuations = new Map([
+    [':', true],
+    [',', true],
+    [';', true],
+    ['"', true],
+    ['`', true],
+    ['~', true],
+    [`'`, true],
+    ['-', true],
+    ['--', true],
+    ['\\', true],
+    ['/', true],
+    ['<', true],
+    ['>', true],
+    ['{', true],
+    ['}', true],
+    ['[', true],
+    [']', true],
+    ['+', true],
+    ['-', true],
+    ['*', true],
+    ['_', true],
+    ['(', true],
+    [')', true],
+    ['&', true],
+    ['^', true],
+    ['%', true],
+    ['$', true],
+    ['#', true],
+    ['@', true],
+    ['.', true],
+    ['?', true],
+    ['!', true],
+    ['|', true],
+    ['...', true],
+    ['…', true]
+  ]);
 
-export const Punctuation = {
-  sentencePunctuationRegExp,
-  sentencePunctuationRegExpG,
-  is: (v: string) => punctuations.has(v),
-  /**
-   * 将文本中的中文标点转化为英文标点
-   */
-  normalizeText: (text: string) =>
-    text.replace(/“|”/g, '"')
-      .replace(/‘|’/g, `'`)
-      .replace(/，/g, ',')
-      .replace(/。/g, '.')
-      .replace(/！/g, '!')
-      .replace(/？/g, '?')
-      .replace(/；/g, ';')
-      .replace(/：/g, ':')
-      .replace(/【/g, '[')
-      .replace(/】/g, ']')
-      .replace(/（/g, '(')
-      .replace(/）/g, ')')
-  // 看起来好看点，虽然这两个实际上不是一个符号
-  // .replace(/’/g, '\'')
-};
+  return {
+    sentencePunctuationRegExp,
+    sentencePunctuationRegExpG,
+    is: (v: string) => punctuations.has(v),
+    /**
+     * 将文本中的中文标点转化为英文标点
+     */
+    normalizeText: (text: string) =>
+      text.replace(/“|”/g, '"')
+        .replace(/‘|’/g, `'`)
+        .replace(/，/g, ',')
+        .replace(/。/g, '.')
+        .replace(/！/g, '!')
+        .replace(/？/g, '?')
+        .replace(/；/g, ';')
+        .replace(/：/g, ':')
+        .replace(/【/g, '[')
+        .replace(/】/g, ']')
+        .replace(/（/g, '(')
+        .replace(/）/g, ')')
+    // 看起来好看点，虽然这两个实际上不是一个符号
+    // .replace(/’/g, '\'')
+  };
+})();
 
 // Text
 /**
@@ -259,14 +255,12 @@ export const Punctuation = {
  * TODO: The Text Object should include more text separation methods extracted from AnalyzedResult.
  */
 export const Text = (() => {
-  /**
-   * 2019-1-13 2019-1-13 06:49:59
-   * TODO: Use RegExp Constructors to simplify codes of next two lines.
-   */
-  const separatorRegExp =
-    /(\s+|:|,|"|~|`|(?:--|-)|\\|\/|<|>|\{|\}|\[|\]|\+|\*|_|\(|\)|&|\^|%|\$|#|@|\||’|;|(?:\.{3}|\.)|\?|!|…)/;
-  const separatorRegExpG =
-    /(\s+|:|,|"|~|`|(?:--|-)|\\|\/|<|>|\{|\}|\[|\]|\+|\*|_|\(|\)|&|\^|%|\$|#|@|\||’|;|(?:\.{3}|\.)|\?|!|…)/g;
+  const regExpPattern =
+    // tslint:disable-next-line max-line-length
+    `(\\s+|:|,|"|~|\`|(?:--|-)|\\\\|\\\/|<|>|\\\{|\\\}|\\\[|\\\]|\\\+|\\\*|_|\\\(|\\\)|&|\\\^|%|\\\$|#|@|\\\||’|;|(?:\\\.{3}|\\\.)|\\\?|!|…)`;
+  const separatorRegExp = new RegExp(regExpPattern);
+  const separatorRegExpG = new RegExp(regExpPattern, 'g');
+
   return {
     separatorRegExp, separatorRegExpG,
     isSeparator: (word: string) => !!word.match(separatorRegExp)
